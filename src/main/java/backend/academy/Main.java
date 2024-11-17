25fc6dd56624ae9f41d33825edaef29142b6624d
egfedo <fegor04@gmail.com>
2024-11-17 23:39:54 +0300

    feat: update Main

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,12 @@
 package backend.academy;
 
+import backend.academy.egfedo.Program;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        var program = new Program(System.out, System.err);
+        program.run(args);
     }
 }
