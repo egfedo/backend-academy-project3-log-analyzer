9fedd684bb136991a3b66badc8eb5269b1b9fb7d
egfedo <fegor04@gmail.com>
2024-11-17 23:39:13 +0300

    feat: add missing ReportConfig

---
+++ src/main/java/backend/academy/egfedo/util/ReportConfig.java
@@ -0,0 +1,12 @@
+package backend.academy.egfedo.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ReportConfig {
+    public static final int TOP_N_RESOURCES = 3;
+    public static final int TOP_N_CODES = 5;
+    public static final double MEDIAN_PERCENTILE = 0.5;
+    public static final double PERCENTILE = 0.95;
+    public static final double PERCENTILE_ACCURACY = 0.01;
+}
